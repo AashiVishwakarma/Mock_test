91e79c55d7b9c24f98fa5d70c454570c93132747
Aashi <you@example.com>
2022-09-23 14:27:28 +0530

    added one java file

---
+++ src/main/java/New_Project/Hello.java
@@ -0,0 +1,10 @@
+package Mock_test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello, welcome to the devops batch 6");
+	}
+
+}
